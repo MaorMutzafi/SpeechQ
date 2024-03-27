2cac2241b2dfb61cb9e52b4ab4481b8a604fa50c
MaorMutzafi <73995031+MaorMutzafi@users.noreply.github.com>
2024-03-27 11:47:36 +0200

    add silence

--- Add_sweep_to_speech.py
+++ Add_sweep_to_speech.py
@@ -2,18 +2,19 @@ import numpy as np
 from scipy.io.wavfile import write, read
 from scipy.signal import chirp
 
-def add_sweep_to_speech_dynamic_sr(file_path, sweep_duration, start_freq, end_freq):
+def add_sweep_to_speech_dynamic_sr(file_path, sweep_duration, start_freq, end_freq, silence_duration):
     """
-    Adds a frequency sweep to the beginning of a speech file using the file's sample rate and saves the result.
+    Adds a frequency sweep and a period of silence to the beginning of a speech file using the file's sample rate and saves the result.
 
     Parameters:
     - file_path: Path to the original speech audio file.
     - sweep_duration: Duration of the sweep in seconds.
     - start_freq: Starting frequency of the sweep in Hz.
     - end_freq: Ending frequency of the sweep in Hz.
+    - silence_duration: Duration of the silence in seconds between the sweep and the speech.
 
     Returns:
-    - Path to the resulting audio file with the sweep added.
+    - Path to the resulting audio file with the sweep and silence added.
     """
     # Load the original audio file to get its sample rate
     original_sr, original_signal = read(file_path)
@@ -38,19 +39,24 @@ def add_sweep_to_speech_dynamic_sr(file_path, sweep_duration, start_freq, end_fr
     # Normalize and convert to int16 for WAV format
     sweep_signal_int16 = np.int16(sweep_signal / np.max(np.abs(sweep_signal)) * 32767)
 
-    # Concatenate sweep and original signals
-    combined_signal = np.concatenate((sweep_signal_int16, original_signal_int16))
+    # Generate silence signal based on the silence duration
+    silence_signal_length = int(silence_duration * original_sr)
+    silence_signal = np.zeros(silence_signal_length, dtype=np.int16)
+
+    # Concatenate sweep, silence, and original signals
+    combined_signal = np.concatenate((sweep_signal_int16, silence_signal, original_signal_int16))
 
     # Save the combined signal to a new file
-    params_str = f"_with_sweep_sd{int(sweep_duration)}sec_sf{int(start_freq)}Hz_ef{int(end_freq)}Hz"
+    params_str = f"_with_sweep_sd{int(sweep_duration)}sec_sf{int(start_freq)}Hz_ef{int(end_freq)}Hz_ts{int(silence_duration)}sec"
     output_file_path = file_path.rsplit('.', 1)[0] + params_str + ".wav"
     write(output_file_path, original_sr, combined_signal)
 
     return output_file_path
 
-# Parameters for the sweep
+# Parameters for the sweep and silence
 sweep_duration = 30  # seconds
 start_freq = 200  # Hz
 end_freq = 3000  # Hz
+silence_duration = 10  # seconds, for example
 file_path = 'snds/OSR_us_000_0010_8k.wav'
-output_file_path = add_sweep_to_speech_dynamic_sr(file_path, sweep_duration, start_freq, end_freq)
+output_file_path = add_sweep_to_speech_dynamic_sr(file_path, sweep_duration, start_freq, end_freq, silence_duration)
